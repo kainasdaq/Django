c33926292f6d177c503f8201dd0b11c6c887afbd
kai <kyo6728@gmail.com>
2015-06-10 15:34:19 -0400

    template inheritance, chapter04 finished

--- mySite/mySite/views.py
+++ mySite/mySite/views.py
@@ -15,8 +15,11 @@ def detail(request, my_args):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    #html = "<html><body>It is now %s.</body></html>" % now
+    #return HttpResponse(html)
+    context = {}
+    context['current_date'] = now
+    return render(request, 'current_datetime.html', context)
 
 def hours_ahead(request, offset):
     try:
@@ -24,10 +27,13 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     """ break point to check local var """
-    #assert False
-    ha = datetime.datetime.now() + datetime.timedelta(hours = offset)
-    html = "<html><body>In %s hour(s), it willl be %s.</body></html>" % (offset, ha)
-    return HttpResponse(html)
+    # using 'assert False' as break point to check var values
+    next_time = datetime.datetime.now() + datetime.timedelta(hours = offset)
+    #html = "<html><body>In %s hour(s), it willl be %s.</body></html>" % (offset, next_time)
+    context = {}
+    context['hour_offset'] = offset
+    context['next_time'] = next_time
+    return render(request, 'hours_ahead.html', context) 
 
 def order_confirmation(request):
     """
@@ -43,5 +49,6 @@ def order_confirmation(request):
     order_number = '110-4158206-7371406'
     item_list = {'Filco Ninja Majestouch-2','Cherry','CM Storm QuickFire'}
     express_delivery = True
+    # when using locals(), local variables' names must match those var in the template!
     #return render_to_response( 'order_confirmation.html', locals() )
     return render(request, 'order_confirmation.html', locals())
