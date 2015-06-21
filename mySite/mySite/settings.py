8e06a889e2f24ed5d06326715e7c67f1cb26ddb6
kai <kyo6728@gmail.com>
2015-06-21 17:10:02 -0400

    settings.py use () instead of {} in REST_FRAMEWORK

--- mySite/mySite/settings.py
+++ mySite/mySite/settings.py
@@ -96,14 +96,15 @@ DATABASES = {
 
 # REST
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': {
-        #'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    },
-    'DEFAULT_AUTHENTICATION_CLASSES': {
-        #'rest_framework.authentication.SessionAuthentication',
-        #'rest_framework.authentication.BasicAuthentication',
-        #'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-    }
+    # must be '(' and ')'
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+    ),
 }
 
 # Internationalization
